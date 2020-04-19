fee1fbfdd32a59e838d9baa63a8f03125f29ae8d
Avi Yaniger <avi.yaniger@gmail.com>
2020-04-19 14:39:50 +0300

    designed mobile portrate from home page until training page

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-import React from "react"
-import ReactDOM from "react-dom"
+// Cover page with background image
+let mainHeight = document.querySelector('main').clientHeight
+const screenHeight = document.querySelector('html').clientHeight
 
-import App from "./App"
+if (mainHeight < screenHeight ) {
+    document.querySelector('main').style.height = '100vh'
+}
 
-ReactDOM.render(<App />, document.querySelector("body"))
+// Fit h1 on one line
+const screenWidth = document.querySelector('html').clientWidth
+const h1 = document.querySelector('h1')
+
+if (screenWidth <= 360) {
+    h1.style.fontSize = 'xx-large'
+}
+
+if (screenWidth <= 500) {
+    document.querySelector('#training h1').textContent = 'Training Courses'
+}
